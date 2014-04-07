302d3a77f9c53ea6bf03d82f11774804b7a93a17
rizak <rizak.ahmed@gmail.com>
2014-04-07 11:57:53 -0400

    Module Init

---
+++ src/main/java/org/jahia/services/render/filter/IPFilter.java
@@ -0,0 +1,82 @@
+package org.jahia.services.render.filter;
+
+
+import org.apache.commons.net.util.SubnetUtils;
+import org.jahia.exceptions.JahiaForbiddenAccessException;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.slf4j.Logger;
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pol
+ * Date: 29.05.12
+ * Time: 14:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class IPFilter extends AbstractFilter {
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(IPFilter.class);
+
+
+    public String prepare(RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
+        String currentAddress = renderContext.getRequest().getRemoteAddr();
+        JCRNodeWrapper siteNode = renderContext.getSite();
+        String filterType = siteNode.hasProperty("filterType") ? siteNode.getProperty("filterType").getString() : null;
+        if (filterType != null) {
+            logger.debug("filterType is [" + filterType + "]");
+            if (currentAddress != null) {
+                String ipRangeList = siteNode.hasProperty("ipRangeList") ? siteNode.getProperty("ipRangeList").getString() : null;
+                if (ipRangeList != null) {
+                    boolean inRange = isInRange(currentAddress, ipRangeList);
+                    if ("deny".equals(filterType)) {
+                        if (inRange) {
+                            logger.warn("IPFilter - Deny rule: IP [" + currentAddress + "] is in subnet [" + ipRangeList + "]");
+                            throw new JahiaForbiddenAccessException();
+                        }
+                    } else { // onlyallow
+                        if (!inRange) {
+                            logger.warn("IPFilter - Only Allow rule:  IP [" + currentAddress + "] not in subnet [" + ipRangeList + "]");
+                            throw new JahiaForbiddenAccessException();
+                        }
+                    }
+                }
+            } else {
+                logger.debug("IPFilter - Bypass IP filtering for localhost");
+            }
+        }
+        return null;
+    }
+
+    private boolean isInRange(String address, String ipRangeList) {
+        if (ipRangeList == null || address == null) {
+            return false;
+        }
+        String delimiter = ",";
+        String[] ranges = ipRangeList.split(delimiter);
+        for (String range : ranges) {
+            String subnetCidr = range.trim();
+            if (subnetCidr != null && !"".equals(subnetCidr)) {
+                int idx = subnetCidr.indexOf("/32");
+                if (idx != -1) {
+                    String standaloneIp = subnetCidr.substring(0, idx);
+                    return standaloneIp.equals(address);
+                } else if (subnetCidr.indexOf("/") == -1){
+                    return subnetCidr.equals(address);
+                } else {
+                    try {
+                        SubnetUtils subnetUtils = new SubnetUtils(subnetCidr);
+                        if (subnetUtils.getInfo().isInRange(address)) {
+                            return true;
+                        }
+                    } catch (IllegalArgumentException iae) {
+                        logger.error("Could not parse [" + subnetCidr + "]. Please use as CIDR-notation.");
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
